9ce60ac78ac2ace7df35eee9920949a7b7506e46
Nistrix <142962260+Nistrix@users.noreply.github.com>
2023-08-23 18:17:37 +0530

    Week 3 Task

---
+++ Tasks/Websocket-Chat-Application/src/main/java/io/nextazy/chatapp/websocket/controller/ChatController.java
@@ -0,0 +1,27 @@
+package io.nextazy.chatapp.websocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import io.nextazy.chatapp.websocket.model.ChatMessage;
+
+@Controller
+public class ChatController {
+
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+
+}
---
+++ Tasks/Websocket-Chat-Application/src/test/java/io/nextazy/chatapp/websocket/WebsocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nextazy.chatapp.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
