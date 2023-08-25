d06fd4cb80d3fafd3feb9cb84438ef661b7c20a6
Nistrix <142962260+Nistrix@users.noreply.github.com>
2023-08-25 14:47:55 +0530

    Week 3

---
+++ Tasks/Auction-App-WebSocket/src/test/java/io/nextazy/auction/AuctionAppWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nextazy.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionAppWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
