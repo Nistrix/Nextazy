8b311d73dc38b7c962bab8bb7589b90fa066db0d
Nithish R L <nithish@nextazy.com>
2023-10-26 17:53:32 +0530

    Task 6 ( Web3 ) - Organised

---
+++ Node.js Tasks/Task 6 ( Web3 )/routes/web3Routes.js
@@ -0,0 +1,10 @@
+const sampleWeb3 = require('../services/web3/web3Transaction.js');
+
+const router = require('express').Router();
+
+
+router.get('/createAccount', sampleWeb3.createAccount );
+router.get('/getBalance', sampleWeb3.getBalance);
+router.post('/sendTransaction', sampleWeb3.sendTransaction);
+
+module.exports = router;
